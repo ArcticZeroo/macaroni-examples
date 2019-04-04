e70698799b6b2ad2b5e07aa5e5f497a3875f56c9
Spencer <spencer@frozor.io>
2019-04-04 17:44:31 -0400

    Update macaroni, add string repeater

--- dist/string.js
+++ dist/string.js
@@ -11,5 +11,26 @@ String.prototype[operators.subtract] = function (other) {
   return this.replace(regex, '');
 };
 
+Array.prototype[operators.multiply] = function (other) {
+  if (Operator.strictNotEqual(typeof other, 'number')) {
+    throw new TypeError('Other must be a number to multiply');
+  }
+
+  const repeatedArray = [];
+
+  for (let repeat = 0; Operator.lessThan(repeat, other); repeat = Operator.increment(repeat)) {
+    for (let i = 0; Operator.lessThan(i, this.length); i = Operator.increment(i)) {
+      repeatedArray.push(this[i]);
+    }
+  }
+
+  return repeatedArray;
+};
+
+String.prototype[operators.multiply] = function (other) {
+  return Operator.multiply([this], other).join('');
+};
+
 const baseString = 'hello world!';
 console.log(Operator.subtract(baseString, 'o'));
+console.log(Operator.multiply(baseString, 3));
--- src/string.js
+++ src/string.js
@@ -6,6 +6,27 @@ String.prototype[operators.subtract] = function (other) {
    return this.replace(regex, '');
 };
 
+Array.prototype[operators.multiply] = function (other) {
+   if (typeof other !== 'number') {
+      throw new TypeError('Other must be a number to multiply');
+   }
+
+   const repeatedArray = [];
+
+   for (let repeat = 0; repeat < other; ++repeat) {
+      for (let i = 0; i < this.length; ++i) {
+         repeatedArray.push(this[i]);
+      }
+   }
+
+   return repeatedArray;
+};
+
+String.prototype[operators.multiply] = function (other) {
+   return ([this] * other).join('');
+};
+
 const baseString = 'hello world!';
 
 console.log(baseString - 'o');
+console.log(baseString * 3);
